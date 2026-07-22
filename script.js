2d2774d369f3db766d2cc20faa6e4e7cfc1655b6
mohamedaminhmou-wq <mohamedaminhmou@gmail.com>
2026-07-22 13:09:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function finishLesson() {function finishLesson() {
     alert("🎉 Congratulations! You finished Lesson 1.");
 }
     alert("🎉 Congratulations! You finished Lesson 1.");
+}function checkAnswer() {
+    let answer = document.getElementById("answer").value;
+
+    if (answer === "26") {
+        document.getElementById("result").innerHTML = "✅ Correct!";
+    } else {
+        document.getElementById("result").innerHTML = "❌ Try Again!";
+    }
 }
 function checkAnswer() {
     let answer = document.getElementById("answer").value;
